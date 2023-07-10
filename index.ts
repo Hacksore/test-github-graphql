1e4f73118801c43981d143508857e462f4b9fbbe
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-07-10 09:18:52 -0500

    First commit

---
+++ index.ts
@@ -0,0 +1,37 @@
+import { GraphqlResponseError, graphql } from "@octokit/graphql";
+
+// https://github.com/octokit/graphql.js#typescript
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`,
+  },
+});
+
+const GET_USER_QUERY = `
+  query {
+    viewer {
+      login
+    }
+  }
+`;
+
+try {
+  console.log("getting data...")
+
+  // BRUH: why the fuck i gotta type this manually
+  const res: {
+    viewer: {
+      login: string
+    }
+  } = await graphqlWithAuth(GET_USER_QUERY);
+
+  console.log("got data:", res.viewer.login)
+
+} catch (error: any) {
+  if (error instanceof GraphqlResponseError) {
+    console.log("Request failed:", error.request); // { query, variables: {}, headers: { authorization: 'token secret123' } }
+    console.log(error.message); // Field 'bioHtml' doesn't exist on type 'User'
+  } else {
+    // handle non-GraphQL error
+  }
+}
